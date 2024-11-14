a38f0b9592d7d43e103ab83e3ceac84d91dd185c
gromgrad <snegobur@gmail.com>
2024-11-14 23:09:25 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input("Сегодня лучший день:")
+print(my_string)
+
+print("my_string".upper())
+print("my_string".lower())
+print("my_string".replace("_",""))
+
+name = ("my_string")
+print(name[0])
+
+name = ("my_string")
+print(name[8])
